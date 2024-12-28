9439cc17d1eff3b7eec8e546934eb417b5456dba
salithapr <salithaprabod016@gmail.com>
2024-12-28 11:05:09 +0530

    Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 // script.js
 
 // Function to download the song
-function downloadSong(songFile) {
+function downloadSong(Me Uyane - Hana Shafa.mp3) {
     const link = document.createElement('a'); // Create a new link element
     link.href = songFile; // Set the download file path
     link.download = songFile; // Specify the file name for download
